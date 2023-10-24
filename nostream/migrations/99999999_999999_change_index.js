4487cecf54ce232835bc0ac6b6663f2f720419db
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2023-10-24 16:16:04 -0300

    Add index-dropping migration

---
+++ nostream/migrations/99999999_999999_change_index.js
@@ -0,0 +1,13 @@
+exports.up = async function (knex) {
+  return knex.schema
+    .raw(`DROP INDEX IF EXISTS pubkey_delegator_kind_idx;`)
+    .raw(`CREATE        INDEX pubkey_delegator_kind_idx ON events ( event_pubkey, event_delegator, event_kind );`)
+  ;
+}
+
+exports.down = function (knex) {
+  return knex.schema
+    .raw(`DROP INDEX IF EXISTS pubkey_delegator_kind_idx;`)
+    .raw(`CREATE UNIQUE INDEX pubkey_delegator_kind_idx ON events ( event_pubkey, event_delegator, event_kind );`)
+  ;
+}
